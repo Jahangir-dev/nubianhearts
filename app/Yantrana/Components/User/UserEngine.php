ecf17832ade1a5e6918d0b14118179abbec83bb7
Jahangir-dev <69496697+Jahangir-dev@users.noreply.github.com>
2021-08-11 00:33:07 +0500

    changes

--- app/Yantrana/Components/User/UserEngine.php
+++ app/Yantrana/Components/User/UserEngine.php
@@ -857,7 +857,7 @@ class UserEngine extends BaseEngine
 			
 		 $VuserProfile = $this->userSettingRepository->fetchUserProfile(Auth::user()->_id);
         
-        $profilePictureFolderPath = getPathByKey('profile_photo', ['{_uid}' => $VuserProfile->_uid]);
+        $profilePictureFolderPath = getPathByKey('profile_photo', ['{_uid}' => Auth::user()->_uid]);
         //dd($profilePictureFolderPath,$VuserProfile->_id);
         $userProfilePictureUrl = noThumbImageURL();
         
